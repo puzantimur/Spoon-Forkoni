a34956591a088055bdc0b09f7d8d128328f126a5
Timur Puzan <puzantimur1996@gmail.com>
2022-11-01 16:43:17 +0300

    hard working before exam

---
+++ data/src/main/java/com/example/homew3/cleanArch/data/mapper/GeneralMapper.kt
@@ -0,0 +1,12 @@
+package com.example.homew3.cleanArch.data.mapper
+
+import com.example.homew3.cleanArch.data.model.dto.GeneralDTO
+import com.example.homew3.domain.model.General
+
+internal fun List<GeneralDTO>.toDomainModel(): List<General> = map { it.toDomain() }
+
+internal fun GeneralDTO.toDomain(): General {
+    return General(
+        steps = steps.toDomainListStep()
+    )
+}
